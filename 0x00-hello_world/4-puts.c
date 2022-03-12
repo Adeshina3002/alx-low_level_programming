955279652806a8077c7d7702b66ddef93b993612
Adeshina3002 <oladejishina@gmail.com>
2022-03-12 15:59:42 +0100

    C program that prints out using puts instead of printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
- * Main - Entry point
- * Return: Always m0 (Success)
- */
-
+* main - Entry point
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-       return(0);
-}       
+ puts("Programming is like building a multilingual puzzle\n");
+ return (0);
+}
