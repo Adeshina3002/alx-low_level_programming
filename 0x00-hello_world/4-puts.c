fa7e948e4c56daef217f1420fdc059bd5a9a64f7
Adeshina3002 <oladejishina@gmail.com>
2023-02-16 21:34:45 +0100

    using the puts function instead of the printf

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+/*
+ * This is a program that prints using the puts function
+ * main - Entry point of the program
+ */
+
+#include <stdio.h>
+#include <string.h>
+
+int main () 
+{
+	char str1[] = "Programming is like building a multilingual puzzle";
+
+	puts(str1);
+	return (0);
+}
